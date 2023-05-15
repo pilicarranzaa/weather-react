9c3b9a343f14adce9d9a86bcdb5cf06e082af2cf
pilicarranzaa <115506676+pilicarranzaa@users.noreply.github.com>
2023-05-15 19:14:53 +0200

    default images and settings updated

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import NextDays from "./NextDays";
 import Search from "./Search";
 import Sign from "./Sign";
 
-function App() {
+
+export default function App() {
   return (
-    <div className="body-wrap">
-      <div className="container body defaultBg py-3">
-        <Header />
-        <NextDays />
-        <Search />
-      </div>
-      <Sign />
+      <div className="body-wrap">
+        <div className="container body defaultBg py-3">
+          <Header />
+          <NextDays />
+          <Search />
+        </div>
+        <Sign />
     </div>
   );
 }
-
-export default App;
--- src/NextDays.js
+++ src/NextDays.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles.css";
+import axios from "axios";
 
 export default function NextDays() {
   return (
